52492b537917adc56f18df500d85ea235dc463cd
SarcasticNickname <sarcasticnickname@protonmail.com>
2021-07-23 21:47:11 +0300

    .

---
+++ CustomerManager/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.abc {
+	requires java.persistence;
+	requires javax.servlet.api;
+	requires spring.web;
+	requires spring.webmvc;
+	requires spring.context;
+	requires spring.data.jpa;
+	requires spring.orm;
+	requires spring.tx;
+	requires spring.data.commons;
+	requires spring.beans;
+}
